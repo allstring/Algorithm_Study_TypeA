8645205ee8ca7dfae6ad33ad05ad9505bfeaa746
hj <charmsxda402@gmail.com>
2024-08-08 17:42:43 +0900

    fix: 240808 boj_11501 주식 Solved

--- Hyunju37/week_2/baekjoon_11501.py
+++ Hyunju37/week_2/baekjoon_11501.py
@@ -3,27 +3,13 @@ for _ in range(T):
     N = int(input())
     prices = list(map(int, input().split()))
 
-    #local maxima를 찾아서, 거기에서 이전에 갖고있던 주식을 팔아야함
-    mx = []
 
-    if prices[0] > prices[1]:
-        mx.append(0)
-
-    for i in range(1, N-1):
-        if prices[i-1] < prices[i] > prices[i+1]:
-            mx.append(i)
-
-    if prices[-1] > prices[-2]:
-        mx.append(N-1)
-
-    #print([prices[i] for i in mx])
-
-    buy_idx = 0
     profit = 0
-
-    for i in mx:
-        for j in range(buy_idx, i):
-            profit += (prices[i] - prices[j])
-        buy_idx = i + 1
+    maxima = 0
+    for i in range(N-1, -1, -1):
+        if prices[i] > maxima:
+            maxima = prices[i]
+        else:
+            profit += maxima - prices[i]
 
     print(profit)
