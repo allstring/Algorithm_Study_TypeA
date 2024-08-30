8943aa602238f1cb52de6432b77e173fd8a904d3
조다민 <whekals2@gmail.com>
2024-08-30 11:47:00 +0900

    fix: 240830 boj_1922 [네트워크연결] Solved

--- damin/week1/baekjoon_1922.py
+++ damin/week1/baekjoon_1922.py
@@ -1,13 +1,17 @@
+'''
+가중치 오름차순으로 정렬해 가중치가 최소치인 값들 먼저 연결하기
+값을 연결하면서 연결할 값의 부모가 같으면 사이클이 발생하니 이런 경우는 제외
+'''
 import sys
 sys.setrecursionlimit(100000) # 재귀 깊이 제한 늘리기
 
-def find_set(a):
+def find_set(a): #부모 찾으러 가기
     if vertice[a] != a:
         vertice[a] = find_set(vertice[a])
         return vertice[a]
     return a
 
-def union(a, b):
+def union(a, b): #값 비교하면서 작은쪽에 합치기
     a = find_set(a)
     b = find_set(b)
 
@@ -19,7 +23,7 @@ def union(a, b):
 input = sys.stdin.readline
 computer = int(input())
 input_cnt = int(input())
-vertice = [i for i in range(computer+1)]
+vertice = [i for i in range(computer+1)] #집합 만들기
 edge = [list(map(int, input().split())) for _ in range(input_cnt)]
 edge.sort(key=lambda x: x[2]) # 가중치 오름차순 정렬
 res = 0
