38c1b14035c711e2345143ea8d256d8d462bf50a
nimoot <nimootmic@gmail.com>
2024-09-03 10:30:37 +0900

    fix: 240903 boj_1058 [친구] 주석 수정

--- nimoot/baekjoon_1058.py
+++ nimoot/baekjoon_1058.py
@@ -5,6 +5,7 @@ def floyd_warshall(n, dist):
     for k in range(n):
         for i in range(n):
             for j in range(n):
+                # i에서 j로 바로 가는 것보다 k를 거쳐 가는 경로가 더 짧으면 그 값으로 갱신
                 if dist[i][j] > dist[i][k] + dist[k][j]:
                     dist[i][j] = dist[i][k] + dist[k][j]
 
@@ -29,6 +30,7 @@ for i in range(n):
     count = 0
     for j in range(n):
         if i != j and dist[i][j] <= 2: 
+            # i에서 j로 가는 최단 거리가 2 이하이면 친구로 계산
             count += 1
     max_friends = max(max_friends, count)
 
