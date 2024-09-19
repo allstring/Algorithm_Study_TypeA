e307ff6f1ee4520b5d0e2b40cfd52f341ba9fdb1
JAEHWAN <woghks1213@gmail.com>
2024-09-20 00:25:49 +0900

    add: 240920 boj_1660 [캡틴 이다솜] ing

--- ja-efan/2차/week_3/baekjoon_1660.py
+++ ja-efan/2차/week_3/baekjoon_1660.py
@@ -12,22 +12,23 @@ def main():
     for k in range(3, N+1):
         k_prime_list[k] = int(k_prime_list[k-1] + (k*(k+1)) / 2)
 
-    # print(k_prime_list)
+    print(k_prime_list)
 
     # memoization 배열 초기화
     memo = [[0 for _ in range(N+1)]]
     k = 1
-    while k_prime_list[k] < N:
+    while k_prime_list[k] <= N:
         if k == 1:
             memo.append([i for i in range(N+1)])
         else:
             memo.append([0 for _ in range(N+1)])
         k += 1
-    # print(memo)
+    # memo.append([0 for _ in range(N+1)])
+    print(f"shape: {len(memo), len(memo[0])}")
 
     for k in range(1, len(memo)):
         k_prime = k_prime_list[k]
-        # print(k_prime)
+        print(k_prime)
         for n in range(1, N+1):
             # print(k, n)
             if N < k_prime:
@@ -36,9 +37,9 @@ def main():
                 memo[k][n] = 1
             elif N > k_prime:
                 memo[k][n] = (n // k_prime) + (memo[k-1][n % k_prime])
-            # print(memo)
+        print(memo)
     # print(memo)
-    print(memo[-1][-1])
+    print(memo[-1][N])
     return
 
 if __name__ == "__main__":
