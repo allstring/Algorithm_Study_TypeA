f59b77e89cfc7d78e246e60fd3bffe29b817bf89
nimoot <nimootmic@gmail.com>
2024-09-21 23:55:21 +0900

    fix: 240921 boj_14629 [숫자 조각] 코드 수정

--- nimoot/week_3_4/baekjoon_14629.py
+++ nimoot/week_3_4/baekjoon_14629.py
@@ -5,18 +5,18 @@ def make_nums():
     for length in range(1, 11):  # 숫자는 최대 10자리
         for nums in permutations('0123456789', length):
             numbers.add(int(''.join(nums)))
-    return sorted(numbers)  # 정렬
+    return sorted(numbers)  # 정렬해야 작은 수 반환 가능
 
 target = int(input())
 
 numbers = make_nums()
 
-ans = None
+ans = -1
 min_difference = float('inf')
 
 for number in numbers:
     difference = abs(number - target)
-    if difference < min_difference or (difference == min_difference and (ans is None or number < ans)):
+    if difference < min_difference:
         ans = number
         min_difference = difference
 
