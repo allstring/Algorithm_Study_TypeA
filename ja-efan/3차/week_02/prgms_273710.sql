da900e86d799677ab7d85267176ff5fbfd34cca7
Jaehwan <woghks1213@gmail.com>
2024-10-16 11:29:36 +0900

    fix: 241016 prgms_273710 [ROOT 아이템 구하기] 컨벤션 수정

--- ja-efan/3차/week_02/prgms_273710.sql
+++ ja-efan/3차/week_02/prgms_273710.sql
@@ -9,7 +9,7 @@ FROM
     item_info AS ii
 LEFT JOIN
     item_tree AS it ON ii.item_id = it.item_id
-WHERE 
-    it.parent_item_id IS NULL
+WHERE 1 = 1
+    AND it.parent_item_id IS NULL
 ORDER BY 
     item_id ASC;
