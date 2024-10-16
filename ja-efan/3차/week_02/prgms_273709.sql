ca3cb7dcd193bd0cea241a6a69b4c4023ab599cd
Jaehwan <woghks1213@gmail.com>
2024-10-16 10:10:01 +0900

    fix: 241016 prgms_273709 [조건에 맞는 아이템들의 가격의 총합 구하기] 컨벤션 수정

--- ja-efan/3차/week_02/prgms_273709.sql
+++ ja-efan/3차/week_02/prgms_273709.sql
@@ -2,6 +2,9 @@
 # lv 2
 # 조건에 맞는 아이템들의 가격의 총합 구하기
 
-SELECT SUM(price) AS TOTAL_PRICE
-FROM item_info
-WHERE rarity = 'LEGEND';
+SELECT 
+  SUM(price) AS `total_price`
+FROM 
+  item_info
+WHERE 1 = 1 
+  rarity = 'LEGEND';
