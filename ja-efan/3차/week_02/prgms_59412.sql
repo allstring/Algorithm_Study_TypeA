18768c1dba802aa0974f2c24f82f7d47bf3f8465
Jaehwan <woghks1213@gmail.com>
2024-10-16 09:34:11 +0900

    fix: 241016 prgms_59412 [입양 시각 구하기(1)] 컨벤션 수정

--- ja-efan/3차/week_02/prgms_59412.sql
+++ ja-efan/3차/week_02/prgms_59412.sql
@@ -2,11 +2,20 @@
 # lv 2
 # 입양 시각 구하기 (1)
 
-SELECT HOUR, COUNT(*) AS "COUNT"
-FROM (
-    SELECT *, HOUR(datetime) AS "HOUR"
-    FROM animal_outs
-) AS ao
-WHERE HOUR BETWEEN 9 AND 19
-GROUP BY HOUR
-ORDER BY HOUR;
+SELECT 
+    hour, 
+    COUNT(*) AS `count`
+FROM 
+    (
+        SELECT 
+            *, 
+            HOUR(datetime) AS `hour`
+        FROM 
+            animal_outs
+    ) AS ao
+WHERE 
+    hour BETWEEN 9 AND 19
+GROUP BY 
+    hour
+ORDER BY 
+    hour ASC;
