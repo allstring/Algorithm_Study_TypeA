f84eef7d8774ff42b63cec0a6f6587f795605d53
Jaehwan <woghks1213@gmail.com>
2024-10-16 09:38:27 +0900

    fix: 241016 prgms_131115 [가격이 제일 비싼 식품의 정보 출력하기] 컨벤션 수정

--- ja-efan/3차/week_02/prgms_131115.sql
+++ ja-efan/3차/week_02/prgms_131115.sql
@@ -3,13 +3,28 @@
 # 가격이 제일 비싼 식품의 정보 출력하기
 
 # 정렬 후 LIMIT 사용
-SELECT product_id, product_name, product_cd, category, price  
-FROM food_product
-ORDER BY price DESC
-LIMIT 1;
+SELECT 
+  product_id, 
+  product_name, 
+  product_cd, 
+  category, 
+  price  
+FROM 
+  food_product
+ORDER BY 
+  price DESC
+LIMIT 
+  1;
 
 
 # MAX 사용
-SELECT product_id, product_name, product_cd, category, price  
-FROM food_product
-WHERE price = (SELECT MAX(price) FROM food_product);
+SELECT 
+  product_id, 
+  product_name, 
+  product_cd, 
+  category, 
+  price  
+FROM 
+  food_product
+WHERE 1 = 1 
+  price = (SELECT MAX(price) FROM food_product);
