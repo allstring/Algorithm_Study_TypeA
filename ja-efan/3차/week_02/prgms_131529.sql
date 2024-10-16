7cd8a3ca493e06112973a6c7fcf5f6143cfb514f
Jaehwan <woghks1213@gmail.com>
2024-10-16 09:57:22 +0900

    fix: 241016 prgms_131529 [카테고리 별 상품 개수 구하기] 컨벤션 수정

--- ja-efan/3차/week_02/prgms_131529.sql
+++ ja-efan/3차/week_02/prgms_131529.sql
@@ -2,10 +2,17 @@
 # lv 2
 # 카테고리 별 상품 개수 구하기 
 
-SELECT p.category, COUNT(p.product_id) AS 'product'
+SELECT 
+    p.category, 
+    COUNT(p.product_id) AS `product`
 FROM (
-    SELECT *, SUBSTRING(product_code,1 ,2) AS 'category'
-    FROM product 
+    SELECT 
+        *, 
+        SUBSTRING(product_code,1 ,2) AS 'category'
+    FROM 
+        product 
 ) AS p
-GROUP BY p.category
-ORDER BY p.product_code;
+GROUP BY 
+    p.category
+ORDER BY 
+    p.product_code ASC;
