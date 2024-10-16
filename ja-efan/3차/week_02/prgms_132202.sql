26950fe3594d5fc9c21fb88a8584cc9c1d9e577e
Jaehwan <woghks1213@gmail.com>
2024-10-16 10:03:40 +0900

    fix: 241016 prgms_132202 [진료과별 총 예약 횟수 출력하기] 컨벤션 수정

--- ja-efan/3차/week_02/prgms_132202.sql
+++ ja-efan/3차/week_02/prgms_132202.sql
@@ -2,8 +2,15 @@
 # lv 2
 # 진료과별 총 예약 횟수 출력하기 
 
-SELECT mcdp_cd AS '진료과코드', COUNT(*) AS "5월예약건수"
-FROM appointment
-WHERE apnt_ymd BETWEEN "2022-05-01" AND "2022-05-31"
-GROUP BY mcdp_cd
-ORDER BY `5월예약건수` ASC, `진료과코드` ASC;
+SELECT 
+  mcdp_cd AS `진료과코드`, 
+  COUNT(*) AS `5월예약건수`
+FROM 
+  appointment
+WHERE 1 = 1
+  AND apnt_ymd BETWEEN "2022-05-01" AND "2022-05-31"
+GROUP BY
+  mcdp_cd
+ORDER BY 
+  `5월예약건수` ASC, 
+  `진료과코드` ASC;
