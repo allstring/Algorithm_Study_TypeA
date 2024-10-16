37e3bcd7923fab970b4ea1ee09023a8fd7359ff6
Jaehwan <woghks1213@gmail.com>
2024-10-16 09:30:10 +0900

    fix: 241016 prgms_59410 [NULL 처리하기] 컨벤션 수정

--- ja-efan/3차/week_02/prgms_59410.sql
+++ ja-efan/3차/week_02/prgms_59410.sql
@@ -2,6 +2,11 @@
 # lv 2
 # NULL 처리하기 
 
-SELECT animal_type, IFNULL(name, 'No name'), sex_upon_intake 
-FROM animal_ins 
-ORDER BY animal_id;
+SELECT 
+  animal_type, 
+  IFNULL(name, 'No name'), 
+  sex_upon_intake 
+FROM 
+  animal_ins 
+ORDER BY 
+  animal_id ASC;
